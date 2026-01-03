09df382b35c244203bc29de97a09d4be5db1890a
Kanzurrizk <117378219+kanzur@users.noreply.github.com>
2026-01-04 00:47:12 +0530

    Delete ai_engine directory

--- ai_engine/training/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Training module for tea leaf disease detection."""
-from .trainer import TeaLeafDiseaseTrainer, train_tea_disease_model
-
-__all__ = ["TeaLeafDiseaseTrainer", "train_tea_disease_model"]
